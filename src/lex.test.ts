a691df2db2f5684d8ef0eeb7de0cbb5d90920eb2
SerKo <serko.dev@gmail.com>
2022-04-10 05:20:00 +0800

    test: add front-selector test case

--- src/lex.test.ts
+++ src/lex.test.ts
@@ -29,6 +29,35 @@ test("lex end-selector", () => {
     ].forEach(c => expect(lex(c.in)).toStrictEqual(c.out));
 });
 
+test("lex front-selector", () => {
+    [
+        {
+            in: "_:where(pre,a){font:bass;_asd:sxx}",
+            out: { styles: ["font:bass", "asd:sxx"], selector: "_:where(pre,a)" }
+        },
+        {
+            in: "@xs{p:2;4;_m:8;16}",
+            out: { styles: ["p:2;4", "m:8;16"], selector: "@xs" }
+        },
+        {
+            in: "@xs{font:bass;_asd:sxx}",
+            out: { styles: ["font:bass", "asd:sxx"], selector: "@xs" }
+        },
+        {
+            in: "@xs{font:bass;_content:'a|a'::after}",
+            out: { styles: ["font:bass", "content:'a|a'::after"], selector: "@xs" }
+        },
+        {
+            in: "@xs{font:bass;_content:'a|)}a'::after}",
+            out: { styles: ["font:bass", "content:'a|)}a'::after"], selector: "@xs" }
+        },
+        {
+            in: "@xs{font:bass;_content:'a|)\\'}a'::after}",
+            out: { styles: ["font:bass", "content:'a|)\\'}a'::after"], selector: "@xs" }
+        }
+    ].forEach(c => expect(lex(c.in)).toStrictEqual(c.out));
+});
+
 test("lex escape", () => {
     [
         {
@@ -58,6 +87,14 @@ test("lex escape", () => {
         {
             in: "{content:'\\}';_font:red}",
             out: { styles: ["content:'\\}'", "font:red"], selector: "" }
+        },
+        {
+            in: "_:where(.content:'\\}'){content:'\\}';_font:red}",
+            out: { styles: ["content:'\\}'", "font:red"], selector: "_:where(.content:'\\}')" }
+        },
+        {
+            in: "_:where(.content:'\\''){font:red}",
+            out: { styles: ["font:red"], selector: "_:where(.content:'\\'')" }
         }
     ].forEach(c => expect(lex(c.in)).toStrictEqual(c.out));
 });
