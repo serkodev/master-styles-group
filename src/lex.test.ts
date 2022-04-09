beff6c75cfb778408dfd621f1cafab17941acf51
SerKo <serko.dev@gmail.com>
2022-04-09 17:30:00 +0800

    feat: add lex

---
+++ src/lex.test.ts
@@ -0,0 +1,76 @@
+import lex from "./lex";
+
+test("lex end-selector", () => {
+    [
+        {
+            in: "{font:bass;_asd:sxx}_:where(pre,a)",
+            out: { styles: ["font:bass", "asd:sxx"], selector: "_:where(pre,a)" }
+        },
+        {
+            in: "{font:bass;_asd:sxx}@xs",
+            out: { styles: ["font:bass", "asd:sxx"], selector: "@xs" }
+        },
+        {
+            in: "{font:bass;_content:'a|a'::after}@xs",
+            out: { styles: ["font:bass", "content:'a|a'::after"], selector: "@xs" }
+        },
+        {
+            in: "{font:bass;_content:'a|)}a'::after}@xs",
+            out: { styles: ["font:bass", "content:'a|)}a'::after"], selector: "@xs" }
+        },
+        {
+            in: "{font:bass;_content:'a|)\\'}a'::after}@xs",
+            out: { styles: ["font:bass", "content:'a|)\\'}a'::after"], selector: "@xs" }
+        }
+    ].forEach(c => expect(lex(c.in)).toStrictEqual(c.out));
+});
+
+test("lex escape", () => {
+    [
+        {
+            in: "{content:'foo;_bar';_foo:bar}",
+            out: { styles: ["content:'foo;_bar'", "foo:bar"], selector: "" }
+        },
+        {
+            in: "{content:'a|)\\'}a'}",
+            out: { styles: ["content:'a|)\\'}a'"], selector: "" }
+        },
+        {
+            in: "{content:''}",
+            out: { styles: ["content:''"], selector: "" }
+        },
+        {
+            in: "{content:'\\''}",
+            out: { styles: ["content:'\\''"], selector: "" }
+        },
+        {
+            in: "{content:'\''}",
+            out: undefined
+        },
+        {
+            in: "{content:'}'}",
+            out: { styles: ["content:'}'"], selector: "" }
+        },
+        {
+            in: "{content:'\\}';_font:red}",
+            out: { styles: ["content:'\\}'", "font:red"], selector: "" }
+        }
+    ].forEach(c => expect(lex(c.in)).toStrictEqual(c.out));
+});
+
+test("lex empty", () => {
+    [
+        {
+            in: "{}",
+            out: { styles: [], selector: "" }
+        },
+        {
+            in: "{}@xm",
+            out: { styles: [], selector: "@xm" }
+        },
+        {
+            in: "{foo:bar;_bar:foo}",
+            out: { styles: ["foo:bar", "bar:foo"], selector: "" }
+        }
+    ].forEach(c => expect(lex(c.in)).toStrictEqual(c.out));
+});
---
+++ src/lex.ts
@@ -0,0 +1,60 @@
+function lex(src: string): { styles: string[], selector: string } | undefined {
+    const len = src.length;
+    let pos = 0;
+
+    const styles: string[] = [];
+
+    function findNext(charList: string[]): string | undefined {
+        while (pos < len) {
+            const c = src[pos];
+            if (charList.includes(c)) {
+                return c;
+            }
+            pos++;
+        }
+    }
+
+    if (src[pos] == "{") {
+        // start
+        pos++;
+        let clsPosition = pos;
+
+        while (pos < len) {
+
+            switch (findNext(["'", ";", "}"])) {
+                case "'": // string
+                    pos++;
+                    parseString:
+                    while (pos < len) {
+                        switch (findNext(["'", "\\"])) {
+                            case "\\":
+                                pos++;
+                                break;
+                            case "'":
+                                break parseString;
+                            default:
+                                return; // "error";
+                        }
+                        pos++;
+                    }
+                    break;
+                case ";": // test is end class
+                    if ((pos + 1 < len) && src[pos + 1] == "_") {
+                        styles.push(src.substring(clsPosition, pos++));
+                        clsPosition = pos + 1;
+                    }
+                    break;
+                case "}":
+                    const lastStyle = src.substring(clsPosition, pos++);
+                    if (lastStyle.length > 0) {
+                        styles.push(lastStyle);
+                    }
+                    return { styles, selector: src.substring(pos) };
+                default:
+            }
+            pos++;
+        }
+    }
+}
+
+export default lex;
