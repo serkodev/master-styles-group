69223c248e237c5e061a0a84f78eaf11e943cd53
SerKo <serko.dev@gmail.com>
2022-04-10 05:19:52 +0800

    feat: support front-selector

--- src/lex.ts
+++ src/lex.ts
@@ -3,9 +3,10 @@ export type LexResult = { styles: string[], selector: string }
 function lex(src: string): LexResult | undefined {
     const len = src.length;
 
-    if (src[0] == "{" ) {
+    if (src[0] == "{" || src[len-1] == "}") {
         let pos = 0;
         const styles: string[] = [];
+        let selector = "";
 
         const findNext = (charList: string[]): string | undefined => {
             while (pos < len) {
@@ -17,28 +18,50 @@ function lex(src: string): LexResult | undefined {
             }
         };
 
+        const parseString = () => {
+            while (pos < len) {
+                switch (findNext(["'", "\\"])) {
+                    case "\\":
+                        pos++;
+                        break;
+                    case "'":
+                        return;
+                    default:
+                        return; // "error";
+                }
+                pos++;
+            }
+        };
+
+        // selector on the begin
+        if (src[0] != "{" && src[len-1] == "}") {
+            const parseFrontSelector = () => {
+                while (pos < len) {
+                    switch (findNext(["'", "{"])) {
+                        case "'": // string
+                            pos++;
+                            parseString();
+                            break;
+                        case "{":
+                            selector = src.substring(0, pos);
+                            return true;
+                    }
+                    pos++;
+                }
+                return false;
+            };
+            if (!parseFrontSelector()) return;
+        }
+
         // start
         pos++;
         let clsPosition = pos;
 
         while (pos < len) {
-
             switch (findNext(["'", ";", "}"])) {
                 case "'": // string
                     pos++;
-                    parseString:
-                    while (pos < len) {
-                        switch (findNext(["'", "\\"])) {
-                            case "\\":
-                                pos++;
-                                break;
-                            case "'":
-                                break parseString;
-                            default:
-                                return; // "error";
-                        }
-                        pos++;
-                    }
+                    parseString();
                     break;
                 case ";": // test is end class
                     if ((pos + 1 < len) && src[pos + 1] == "_") {
@@ -54,7 +77,7 @@ function lex(src: string): LexResult | undefined {
                     if (lastStyle.length > 0) {
                         styles.push(lastStyle);
                     }
-                    return { styles, selector: src.substring(pos) };
+                    return { styles, selector: selector || src.substring(pos) };
                 default:
             }
             pos++;
